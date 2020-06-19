4a25e9bdaeb03d6723905a35390712753816a605
malyz <mattuy001@gmail.com>
2020-06-19 18:37:27 +1200

    added AuthController

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AuthController
+  {
+    private readonly IAuthRepository _repo;
+    public AuthController(IAuthRepository repo)
+    {
+      _repo = repo;
+
+    }
+  }
+}
