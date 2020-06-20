0e9c8c16fac9b3b8c87caba78bfa3b86b1d1a0ff
malyz <mattuy001@gmail.com>
2020-06-20 13:13:02 +1200

    used userForRegisterDto in register post request

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -2,7 +2,7 @@ namespace DatingApp.API.Dtos
 {
   public class UserForRegisterDto
   {
-    public string Usernam { get; set; }
+    public string Username { get; set; }
     public string Password { get; set; }
   }
 }
