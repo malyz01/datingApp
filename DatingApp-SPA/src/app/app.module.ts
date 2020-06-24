3fe3224b3305fa81307231a73adaf1d957c969d4
malyz <mattuy001@gmail.com>
2020-06-24 20:12:45 +1200

    added navbar

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { AnotherComponent } from './another/another.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       ValueComponent,
-      AnotherComponent
+      AnotherComponent,
+      NavComponent
    ],
    imports: [
       BrowserModule,
